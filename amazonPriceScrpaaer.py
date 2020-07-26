2d7c3693e4312b8cbd1611d4fcb1a842b1bfa765
astitvaOYO <astitva.sood@oyorooms.com>
2020-07-27 02:32:09 +0530

    pass-url-by-command-line-argument

--- amazonPriceScrpaaer.py
+++ amazonPriceScrpaaer.py
@@ -1,7 +1,7 @@
 import requests
 import bs4
-
-URL = "https://www.amazon.in/Dell-Screen-LED-Lit-Monitor-P2419H/dp/B07F8XZN69/ref=sr_1_2?dchild=1&keywords=dell+p2419h&qid=1595792667&sr=8-2"
+import sys
+URL = sys.argv[1]
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
 
